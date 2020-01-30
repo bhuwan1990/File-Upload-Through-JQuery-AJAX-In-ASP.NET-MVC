3718a9139f9efe6d530586ad43839113c6aec42f
bhuwan1990 <37440900+bhuwan1990@users.noreply.github.com>
2020-01-30 23:45:29 +0530

    Initial Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult UploadFiles()
+        {
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";
+                        string filename = Path.GetFileName(Request.Files[i].FileName);
+
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+
+                        fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                        file.SaveAs(fname);
+
+                        string userName = Request.Form["txtUserName"];
+                        string phoneNo = Request.Form["txtPhoneNo"];
+
+                        if (userName.Equals("Manas", StringComparison.CurrentCultureIgnoreCase)
+                        && phoneNo.Equals("9007696946", StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            return Json("Data saved successful !");
+                        }
+                        else
+                        {
+                            return Json("Save failed !");
+                        }
+                    }
+
+                    return Json("File Uploaded Successfully!");
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+            {
+                return Json("No files selected.");
+            }
+        }
+    }
+}
